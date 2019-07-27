ac537c7a0b0b7bf3475de442c2d435d25151eb7f
Karl Bullock <karlbullock@karls-mbp.home>
2019-07-27 14:42:33 +0100

    fixed error on compile.js

--- compile.js
+++ compile.js
@@ -5,4 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(SVGPathSegCurvetoCubicRel.OfflineAudioCompletionEvent(source, 1));
+console.log(solc.compile(source, 1));
